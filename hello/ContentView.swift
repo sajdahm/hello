92a549d37050571535add7f9ffb9ccbab13db7a8
smuhammad <sajdahmuhammad26@gmail.com>
2022-09-29 13:34:13 -0400

    Initial Commit

---
+++ hello/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello
+//
+//  Created by Sajdah Muhammad on 9/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by Sajdah Muhammad on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
